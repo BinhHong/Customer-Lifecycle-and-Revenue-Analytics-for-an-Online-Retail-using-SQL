d2b16185839bbe7e28a3202482bdf21ca9978973
Binh Hong Ngoc <binh.hongngoc@gmail.com>
2026-06-27 20:01:04 +0200

    customer metrics completed

--- SQL codes/05_customer_metrics.sql
+++ SQL codes/05_customer_metrics.sql
@@ -22,23 +22,21 @@ FROM base_customers;
 SELECT ROUND(AVG(total_revenue),2) as avg_revenue
 FROM base_customers;
 
--- Average oders per customer
+-- Average orders per customer
 -- Result: 6.25
 SELECT ROUND(AVG(total_orders),2) as avg_orders
 FROM base_customers;
 
--- Average Oder Value (AOV)
+-- Average Order Value (AOV)
 -- Result: 466.43
 SELECT ROUND(SUM(total_revenue)/SUM(total_orders),2) AS aov
 FROM base_customers;
-
-SELECT AVG(avg_order_value)
-FROM base_customers; -- 384.4049171365
 
 -- Average customer lifetime (days)
 -- Result: 273.81
 SELECT ROUND(AVG(active_days),2) AS avg_lifetime
 FROM base_customers;
+
 
 /*==================================================
    2. Customer Acquisition Analysis
@@ -46,35 +44,37 @@ FROM base_customers;
   
 -- New customers per month
 -- Result: 
-SELECT first_order_month, COUNT(*) AS num_customers_month
+SELECT first_order_month, COUNT(*) AS customer_acquisition
 FROM base_customers
 GROUP BY first_order_month
 ORDER BY first_order_month;
 
 -- Month with highest acquisition
 -- Result: 2009-12 with 951 new customers
-WITH num_cus_month AS
-(SELECT first_order_month, COUNT(*) AS num_customers_month
+WITH monthly_acquisition AS
+(SELECT first_order_month, COUNT(*) AS customer_acquisition
 FROM base_customers
 GROUP BY first_order_month
 ORDER BY first_order_month
 )
-SELECT first_order_month, num_customers_month
-FROM num_cus_month
-WHERE num_customers_month = 
-	(SELECT MAX(num_customers_month)
-    FROM num_cus_month
+SELECT first_order_month, customer_acquisition
+FROM monthly_acquisition
+WHERE customer_acquisition = 
+	(SELECT MAX(customer_acquisition)
+    FROM monthly_acquisition
     );
 
--- Cumulative Number of customers and fluctuations between months
-WITH num_cus_month AS
-(SELECT first_order_month, COUNT(*) AS num_customers_month
+-- Cumulative number of customers and fluctuations between months
+WITH monthly_acquisition AS
+(SELECT first_order_month, COUNT(*) AS customer_acquisition
 FROM base_customers
 GROUP BY first_order_month
 ORDER BY first_order_month
 )
 SELECT *, 
-	SUM(num_customers_month) OVER(ORDER BY first_order_month) AS cumulative_customers,
-    LAG(num_customers_month) OVER(ORDER BY first_order_month) AS last_num_customers,
-    num_customers_month - LAG(num_customers_month) OVER(ORDER BY first_order_month) AS num_customer_diff
-FROM num_cus_month;
+	SUM(customer_acquisition) OVER(ORDER BY first_order_month) AS cumulative_customers,
+    LAG(customer_acquisition) OVER(ORDER BY first_order_month) AS previous_month_customers,
+    customer_acquisition - LAG(customer_acquisition) OVER(ORDER BY first_order_month) AS customer_growth,
+    ROUND((customer_acquisition - LAG(customer_acquisition) OVER(ORDER BY first_order_month))/LAG(customer_acquisition) OVER(ORDER BY first_order_month)*100,2) 
+		AS 'Growth Rate (%)'
+FROM monthly_acquisition;
